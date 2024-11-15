13b6da7a5d13010d8c4adbd6ce2667177a84a22f
Per Arnborg <kontakt@perarnborg.se>
2024-11-15 13:54:22 +0100

    Change rest permission callback

--- public/ahw-ai-api-endpoints.php
+++ public/ahw-ai-api-endpoints.php
@@ -3,6 +3,8 @@ add_action( 'rest_api_init', function () {
   register_rest_route( AKKA_API_BASE, '/ai/meda_description/(?P<post_id>[0-9]+)', array(
     'methods' => 'GET',
     'callback' => 'Akka_headless_wp_ai::get_meta_description',
-    'permission_callback' => 'Akka_headless_wp_content::can_get_content',
+    'permission_callback' => function () {
+      return current_user_can("edit_posts");
+    },
   ) );
 } );
